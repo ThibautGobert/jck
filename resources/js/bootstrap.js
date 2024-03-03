19c850733b1bb131dbb587765eddffe30f9fc990
Thibaut <tg@bms-engineering.lu>
2024-03-03 21:42:41 +0100

    fix cesium

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,6 +16,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import Echo from 'laravel-echo';
+import * as Cesium from "cesium";
+window.Cesium = Cesium
 
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
